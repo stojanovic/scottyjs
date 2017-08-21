9a3c89eb977e8d8d52e51267f2cd3ba968844337
Christian Kaatz <christian.kaatz@here.com>
2017-08-21 14:48:36 +0200

    WIP: add dns creation support via upsert

--- bin/scotty.js
+++ bin/scotty.js
@@ -50,7 +50,9 @@ function showHelp() {
     ${colors.magenta('--source')}  ${colors.cyan('or')} ${colors.magenta('-s')}    Source of the folder that will be uploaded ${colors.cyan('| default: current folder')}
     ${colors.magenta('--bucket')}  ${colors.cyan('or')} ${colors.magenta('-b')}    Name of the S3 bucket ${colors.cyan('| default: name of the current folder')}
     ${colors.magenta('--region')}  ${colors.cyan('or')} ${colors.magenta('-r')}    AWS region where the files will be uploaded ${colors.cyan('| default: saved region if exists or a list to choose one if it is not saved yet')}
-    ${colors.magenta('--force')}   ${colors.cyan('or')} ${colors.magenta('-f')}    Update the bucket without asking, region can be overridden with ${colors.magenta('-r')} ${colors.cyan('| default: false')} 
+    ${colors.magenta('--zone')}    ${colors.cyan('or')} ${colors.magenta('-z')}    AWS Route53 ZoneId of the dns delegation ${colors.cyan('| default: false')}
+    ${colors.magenta('--name')}    ${colors.cyan('or')} ${colors.magenta('-n')}    The owned url name ${colors.cyan('| default: false')}
+    ${colors.magenta('--force')}   ${colors.cyan('or')} ${colors.magenta('-f')}    Update the bucket without asking, region can be overridden with ${colors.magenta('-r')} ${colors.cyan('| default: false')}
     ${colors.magenta('--update')}  ${colors.cyan('or')} ${colors.magenta('-u')}    Update existing bucket ${colors.cyan('| default: false')}
 
     ✤ ✤ ✤
@@ -72,14 +74,18 @@ function readArgs() {
       s: 'source',
       b: 'bucket',
       r: 'region',
+      z: 'zone',
+      n: 'name',
       f: 'force',
       u: 'update'
     },
-    string: ['source', 'bucket', 'region'],
+    string: ['source', 'bucket', 'region', 'zone', 'name'],
     boolean: ['quiet', 'website', 'spa', 'force', 'update'],
     default: {
       source: process.cwd(),
-      bucket: path.parse(process.cwd()).name
+      bucket: path.parse(process.cwd()).name,
+      zone: false,
+      name: false
     }
   })
 }
@@ -160,7 +166,7 @@ function cmd(console) {
 }
 
 function beamUp (args, region, console) {
-  return scotty(args.source, args.bucket, region, args.website, args.spa, args.update, args.force, args.quiet, console)
+  return scotty(args.source, args.bucket, region, args.website, args.spa, args.update, args.zone, args.name, args.force, args.quiet, console)
     .then(endpoint => clipboardy.write(endpoint))
     .then(() => process.exit(1))
     .catch(() => process.exit(1))
