f3e20a60c621d5ef7af15b344674a8f6f645118b
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2018-02-04 18:18:19 +0100

    [WIP] New 'create' command

--- bin/cmd.js
+++ bin/cmd.js
@@ -7,30 +7,48 @@ const scotty = require('../lib/scotty')
 function readArgs() {
   return minimist(process.argv.slice(2), {
     alias: {
-      h: 'help',
-      v: 'version',
-      q: 'quiet',
-      w: 'website',
-      s: 'source',
       b: 'bucket',
-      r: 'region',
-      f: 'force',
-      u: 'update',
       d: 'delete',
-      i: 'interactive'
+      f: 'force',
+      h: 'help',
+      i: 'interactive',
+      q: 'quiet',
+      r: 'region',
+      s: 'source',
+      u: 'update',
+      v: 'version',
+      w: 'website'
     },
-    string: ['source', 'bucket', 'region'],
-    boolean: ['quiet', 'website', 'spa', 'force', 'update', 'delete', 'interactive'],
+    string: ['source', 'bucket', 'region', 'config'],
+    boolean: [
+      'delete',
+      'force',
+      'interactive',
+      'quiet',
+      'reuse',
+      'spa',
+      'update',
+      'website'
+    ],
     default: {
       source: process.cwd(),
-      bucket: path.parse(process.cwd()).name
+      bucket: path.parse(process.cwd()).name,
+      config: path.join(__dirname, 'scotty.json')
     }
   })
 }
 
 function cmd() {
   const args = readArgs()
-  // const command = args._ && args._.length && args._[0]
+  const command = args._ && args._.length && args._[0]
+
+  if (command === 'destroy') {
+    console.log('Destroy current scotty build')
+  }
+
+  if (args.help) {
+    console.log('Help, command:', command)
+  }
 
   if (args.version) {
     const packageJson = require(path.join(__dirname, '..', 'package.json'))
@@ -41,13 +59,17 @@ function cmd() {
     // Run interactive scotty command
   }
 
-  console.log(args)
+  // console.log(args)
 
   scotty({
+    bucket: args.bucket,
+    config: args.config,
     log: !args.quiet,
+    region: args.region,
+    source: args.source,
     spa: args.spa,
     website: args.website
-  })
+  }, console.log)
 }
 
 if (require.main === module)
